3865e9aafb58050f88b87f83f9d1aa4106874ddf
Xyven1 <maritimus987@gmail.com>
2022-04-06 13:54:55 -0400

    update about me page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -19,8 +19,8 @@
 </svelte:head>
 <h1 class="text-2xl m-auto text-center">
 	I am currently pursuing my bachelor's degree in Computer Science at WPI, with plans to complete a
-	4 year BS/MS program with a focus on ML. I have been coding for nearly 10 years, starting with
-	software for Arduino and have been coding avidly since. Nearly all of my programming knowledge is
-	self-taught through personal projects and lectures on YouTube, and I have learned new languages,
-	techniques, and skills quickly and without help from others.
+	4 year BS/MS program with a focus on ML. I start coding nearly 10 years ago, starting with
+	software for Arduino and have been coding avidly since. Most of my programming knowledge is
+	self-taught through personal projects and lectures on YouTube. I have devloped a strong ability to
+  learn new languages, techniques, and skills quickly and without help from others.
 </h1>
