5fecac4477479835e0b8980e0b0bd8cae9fe9158
xyven1 <git@xyven.dev>
2023-10-29 20:17:31 -0400

    default error page, fix dev hmr, better font load

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+    {
+      postcssPlugin: true,
+      Declaration: {
+        'font-display': (node) => {
+          if (node.parent.name === 'font-face' && node.parent.type === 'atrule') node.value = 'fallback'
+        }
+      }
+    }
+  ],
 }
---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex flex-col items-center justify-center">
+	<h2 class="text-3xl">
+		{$page.error?.message || $page.error}
+	</h2>
+	<h3 class="text-center text-xl">
+		isValidPath("{$page.url.pathname}") == false
+		<br />
+		<a href="/">navigate("/")</a>
+	</h3>
+</div>
