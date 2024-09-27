9438065f0b1ca3b4c0a18f456d91b5a62510f60e
xyven1 <git@xyven.dev>
2024-09-27 00:07:21 -0400

    fix contact api

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -9,6 +9,8 @@ export const load: PageServerLoad = async ({ url }) => {
 				'xyven.dev': '5b3ec125-a470-42d7-bda4-1b2f2dc124aa',
 				'personal-website-svelte.pages.dev': '5b3ec125-a470-42d7-bda4-1b2f2dc124aa',
 				'blake.bruell.com': 'eb0d973f-8562-4ac4-a138-8488d6fac2a0'
-			}[url.hostname] || ''
+			}[url.hostname] || url.hostname.endsWith('personal-website-svelte.pages.dev')
+				? '1cb1191f-2cf4-4cb8-a895-2b83e91a2be5'
+				: ''
 	};
 };
