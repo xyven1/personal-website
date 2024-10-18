d63dd926693c9f0d8fb0d88c46cb5da0398215f4
xyven1 <git@xyven.dev>
2024-10-18 15:10:28 -0400

    cleanup dates

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -4,6 +4,8 @@
 	import { onMount } from 'svelte';
 
 	export let data;
+	if (data.meta.history.length < 1) throw Error('Article must have history');
+
 	const source = `https://github.com/xyven1/personal-website/blob/prod/articles/${data.meta.slug}.md`;
 
 	const anchors: HTMLElement[] = [];
@@ -21,6 +23,23 @@
 		}
 	}
 	let prevWindowWidth = 0;
+
+	const dateStyle = 'medium';
+	const timeStyle = 'short';
+	function dateToDateString(date: string) {
+		return new Date(date).toLocaleDateString('en-US', {
+			timeZone: 'EST',
+			dateStyle
+		});
+	}
+	function dateToDateTimeString(date: string) {
+		return new Date(date).toLocaleString('en-US', {
+			timeZone: 'EST',
+			dateStyle,
+			hour12: false,
+			timeStyle
+		});
+	}
 
 	onMount(() => {
 		anchors.push(
@@ -73,53 +92,51 @@
 	<section class="border-b-[3px] border-neutral-800 pb-2">
 		<h1 class="text-4xl sm:text-5xl">{data.meta.title}</h1>
 		<p class="flex flex-wrap gap-x-2 text-stone-600 dark:text-neutral-400">
-			<span>{new Date(data.meta.date).toLocaleDateString()}</span>
+			<span>{dateToDateString(data.meta.date)}</span>
 			{#if data.meta.updated}
-				<span>(<i>Updated: {new Date(data.meta.updated).toLocaleDateString()}</i>)</span>
+				<span>(<i>Updated: {dateToDateString(data.meta.updated)}</i>)</span>
 			{/if}
 			<span>~ {data.meta.readTime} min read</span>
 		</p>
-		{#if data.meta.history.length > 0}
-			<div class="group">
-				<label
-					class="flex cursor-pointer items-center text-stone-600 hocus:text-accent dark:text-neutral-400 hocus:dark:text-daccent"
-					for="history"
-				>
-					<input type="checkbox" id="history" hidden />
-					<span>History</span>
-					<Icon
-						path={mdiChevronRight}
-						size={1.35}
-						class="transition-[transform] group-focus-within:rotate-90 group-has-[input:checked]:rotate-90"
-					/>
-				</label>
-				<div
-					class="grid grid-rows-[0fr] transition-[grid-template-rows] group-focus-within:grid-rows-[1fr] group-has-[input:checked]:grid-rows-[1fr]"
-				>
-					<div class="overflow-hidden">
-						<ol class="px-1">
-							{#each data.meta.history as commit}
-								<li
-									class="mb-3 border-l-2 border-stone-600 px-1 leading-snug dark:border-neutral-400"
-								>
-									<div>
-										{new Date(commit.date).toLocaleString()} - {commit.message}
-									</div>
-									<div class="space-x-1">
-										{#if commit.liveUrl !== ''}
-											<a href={`${commit.liveUrl}`}> Live Link </a>
-										{/if}
-										<a href={`https://github.com/xyven1/personal-website/commit/${commit.hash}`}>
-											View Diff
-										</a>
-									</div>
-								</li>
-							{/each}
-						</ol>
-					</div>
+		<div class="group">
+			<label
+				class="flex cursor-pointer items-center text-stone-600 hocus:text-accent dark:text-neutral-400 hocus:dark:text-daccent"
+				for="history"
+			>
+				<input type="checkbox" id="history" hidden />
+				<span>History</span>
+				<Icon
+					path={mdiChevronRight}
+					size={1.35}
+					class="transition-[transform] group-focus-within:rotate-90 group-has-[input:checked]:rotate-90"
+				/>
+			</label>
+			<div
+				class="grid grid-rows-[0fr] transition-[grid-template-rows] group-focus-within:grid-rows-[1fr] group-has-[input:checked]:grid-rows-[1fr]"
+			>
+				<div class="overflow-hidden">
+					<ol class="px-1">
+						{#each data.meta.history as commit}
+							<li
+								class="mb-3 border-l-2 border-stone-600 px-1 leading-snug dark:border-neutral-400"
+							>
+								<div>
+									{dateToDateTimeString(commit.date)} - {commit.message}
+								</div>
+								<div class="space-x-1">
+									{#if commit.liveUrl !== ''}
+										<a href={`${commit.liveUrl}`}> Live Link </a>
+									{/if}
+									<a href={`https://github.com/xyven1/personal-website/commit/${commit.hash}`}>
+										View Diff
+									</a>
+								</div>
+							</li>
+						{/each}
+					</ol>
 				</div>
 			</div>
-		{/if}
+		</div>
 
 		<div class="flex flex-wrap gap-2">
 			<div class="mr-auto flex flex-wrap gap-2">
